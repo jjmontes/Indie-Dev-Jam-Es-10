ecc96761fcf37771e0cfc903700c4829a84b2c4f
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2020-06-01 19:36:16 -0300

    Se agrega movimiento al Player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,7 +1,25 @@
 extends KinematicBody2D
 
+var walk_speed = 200
+var jump_speed = -400
+var gravity = 1200
+var jumping = false
+var velocity = Vector2()
+
+func _get_input():
+	velocity.x = walk_speed
+	if Input.is_key_pressed(KEY_SPACE) && !jumping && is_on_floor():
+		jumping = true
+		velocity.y = jump_speed
+	elif Input.is_key_pressed(KEY_CONTROL):
+		print ("punch")
+
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	pass
+	_get_input()
+	velocity.y += delta * gravity
+	if jumping && is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2.UP)
