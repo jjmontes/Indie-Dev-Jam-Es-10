8a089cecd97ffac14e1b6ed91471a2bc23f5bd4c
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2020-06-06 16:42:43 -0300

    Se valida que no haya más niveles y se imprime en consola que el juego termino

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
 var current_level
+var index_level = 0
 var player = preload("res://Scenes/Player.tscn")
 var levels = [
 	preload("res://Scenes/Levels/001.tscn")
 ]
 
 func _ready():
-	load_level(0)
+	load_level(index_level)
 
 func _on_level_exit():
-	print("El jugador salio del nivel... cargando nivel 2")
 	current_level.queue_free()
+	load_level(index_level + 1)
 
-func load_level(level_index):
-	current_level = levels[level_index].instance()
+func load_level(index):
+	if levels.size() <= index:
+		print("Se acabaron los niveles... digamos que ganaste...")
+		return
+	current_level = levels[index].instance()
 	current_level.connect("exit", self, "_on_level_exit")
 	current_level.add_player(player.instance())
 	add_child(current_level)
+	index_level = index
