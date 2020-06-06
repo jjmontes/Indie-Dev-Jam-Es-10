3ca041580fcc5dda31a31f5e9f108378755ab36a
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2020-06-06 16:31:41 -0300

    Se carga el nivel 1 por código

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,5 +1,20 @@
 extends Node2D
 
-func _on_Level_exit():
+var current_level
+var player = preload("res://Scenes/Player.tscn")
+var levels = [
+	preload("res://Scenes/Levels/001.tscn")
+]
+
+func _ready():
+	load_level(0)
+
+func _on_level_exit():
 	print("El jugador salio del nivel... cargando nivel 2")
-	$Level.queue_free()
+	current_level.queue_free()
+
+func load_level(level_index):
+	current_level = levels[level_index].instance()
+	current_level.connect("exit", self, "_on_level_exit")
+	current_level.add_player(player.instance())
+	add_child(current_level)
--- Scripts/Levels/001.gd
+++ Scripts/Levels/001.gd
@@ -4,3 +4,8 @@ signal exit
 
 func _on_Exit_player_entered():
 	emit_signal("exit")
+
+func add_player(player):
+	if player != null && player.is_in_group("player"):
+		player.position = $PlayerStartPosition.position
+		add_child(player)
