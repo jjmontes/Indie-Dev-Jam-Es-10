4f911998a8bdaaa5ca59ed472949b21493e46e96
Juan José Montes de Oca Arbós <juanjose.montesdeocaarbos@gmail.com>
2020-06-27 20:01:55 -0300

    Se modifica estética de salida del nivel

--- Scripts/Exit.gd
+++ Scripts/Exit.gd
@@ -2,6 +2,9 @@ extends Area2D
 
 signal player_entered
 
+func _ready():
+	$AnimationPlayer.play("exit")
+
 func _on_Exit_body_entered(body):
 	if body.is_in_group("player"):
 		emit_signal("player_entered")
